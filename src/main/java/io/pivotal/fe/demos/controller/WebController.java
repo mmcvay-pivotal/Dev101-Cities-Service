47688404572f54b3920efd4326d89670c25d794d
Sufyaan Kazi <skazi@pivotal.io>
2016-04-07 17:42:21 +0100

    Initial skeleton code

---
+++ src/main/java/io/pivotal/fe/demos/controller/WebController.java
@@ -0,0 +1,13 @@
+package io.pivotal.fe.demos.controller;
+
+/**
+ * Class to add optional non-HATEOS and non-pageable list of all cities
+ * @author skazi
+ *
+ */
+public class WebController {
+	
+	public String makeMeAWebService() {
+		return "Hello World";
+	}
+}
---
+++ src/test/java/io/pivotal/fe/demos/TestCityRepository.java
@@ -0,0 +1,22 @@
+package io.pivotal.fe.demos;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Test inspired by:
+ * http://www.jayway.com/2014/07/04/integration-testing-a-spring-boot-
+ * application/
+ * https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-
+ * testing.html
+ * 
+ * @author skazi
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SBootCitiesServiceApplication.class)
+//@WebAppConfiguration
+@WebIntegrationTest({"server.port:0", "eureka.client.enabled:false"})
+public class TestCityRepository {
+}
