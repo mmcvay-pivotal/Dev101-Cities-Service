0feab60eff95843662f65bf49cf84d2db5faff8a
Sufyaan Kazi <skazi@pivotal.io>
2016-04-12 19:55:24 +0100

    Corrected Inital version of app

--- src/main/java/io/pivotal/fe/demos/controller/WebController.java
+++ src/main/java/io/pivotal/fe/demos/controller/WebController.java
@@ -1,13 +1,41 @@
 package io.pivotal.fe.demos.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.pivotal.fe.demos.domain.City;
+
 /**
- * Class to add optional non-HATEOS and non-pageable list of all cities
+ * Class to return a list of cities
  * @author skazi
  *
  */
+@RestController
 public class WebController {
+	private List<City> listOfCities = new ArrayList<>();
 	
-	public String makeMeAWebService() {
-		return "Hello World";
+	public WebController() {
+		listOfCities.add(new City("London","London",null,null));
+		listOfCities.add(new City("Guildford","Surrey",null,null));
+		listOfCities.add(new City("Farnborough","Hampshire",null,null));
+	}
+	
+	@RequestMapping("/cities_all")
+	public Iterable<City> showCities() {
+		return listOfCities;
+	}
+	
+	@RequestMapping("/city")
+	public City showCity(@RequestParam(value="cityToShow", required=false, defaultValue="London") String city) {
+		for(City temp: listOfCities) {
+			if(temp.getName().equals(city)) {
+				return temp;
+			}
+		}
+		return null;
 	}
 }
